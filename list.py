3a4f71054a3afcd37c103e74b1b8a78cbf50f7ac
sebastiancarbone <seba.coal@gmail.com>
2024-11-20 09:54:23 +0100

    list

---
+++ list.py
@@ -0,0 +1,7 @@
+def sum_list(my_list):  #definisco funzione
+    if not my_list:     
+        return None
+    return sum(my_list)
+#test per vedere se funziona con numeri
+#lista = [1,2]
+#print(sum_list(lista))
