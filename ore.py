98cdba2792b7d663f34daf7ce8d0db847058ee55
sebastiancarbone <seba.coal@gmail.com>
2024-11-20 09:44:49 +0100

    ore

--- ore.py
+++ ore.py
@@ -2,5 +2,5 @@ totale_minuti = 538
 
 ore = totale_minuti // 60 #inserisco la doppia / per indicare che è un intero altrimenti sarebbe: ore= int(totale_minuti/60)
 minuti = totale_minuti % 60 #per semplificare il tutto posso usare: ore, minuti = divmod(totale_minuti, 60)
-                            #divmod restituisce sia risultato che modulo della doivisione
+                            #divmod restituisce sia risultato che modulo della divisione
 print(f"{ore}h:{minuti}min")
